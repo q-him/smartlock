1a965b0b2bbac28f07dba811291c1d1a73ee9aa4
Artyom Kozyrev <39848982+q-him@users.noreply.github.com>
2022-12-07 20:47:06 +0300

    basic rfid functionality

---
+++ components/rfid/rfid.c
@@ -0,0 +1,35 @@
+#include "./include/rfid.h"
+
+static rc522_handle_t scanner;
+
+static void rc522_handler(void* arg, esp_event_base_t base, int32_t event_id, void* event_data)
+{
+    rc522_event_data_t* data = (rc522_event_data_t*) event_data;
+
+    switch(event_id) {
+        case RC522_EVENT_TAG_SCANNED: {
+                rc522_tag_t* tag = (rc522_tag_t*) data->ptr;
+                ESP_LOGI(RFID_TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+                LkcMsg_t message = {
+                    .id = LKC_OPEN,
+                    .data = tag->serial_number
+                };
+                xQueueSend(lkc_input, &message, portMAX_DELAY);
+            }
+            break;
+    }
+}
+
+void start_rfid_task() {
+    rc522_config_t config = {
+        .spi.host = VSPI_HOST,
+        .spi.miso_gpio = CONFIG_SPI_MISO_PIN,
+        .spi.mosi_gpio = CONFIG_SPI_MOSI_PIN,
+        .spi.sck_gpio = CONFIG_SPI_SCK_PIN,
+        .spi.sda_gpio = CONFIG_SPI_SDA_PIN,
+    };
+
+    rc522_create(&config, &scanner);
+    rc522_register_events(scanner, RC522_EVENT_ANY, rc522_handler, NULL);
+    rc522_start(scanner);
+}
